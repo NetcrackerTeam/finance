64d4ff145708b0c00717355023203f030a1f4d90
Dmitriy <Summerxwow@gmail.com>
2019-11-06 21:53:30 +0200

    sdsd

--- src/main/java/com/netc/HwController.java
+++ src/main/java/com/netc/HwController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HwController {
+
     @GetMapping( "/" )
     public String welcomePage(Model model) {
         model.addAttribute("hello", "HELLO WORLD");
