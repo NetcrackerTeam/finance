a941f59c85df38e130ca99139bfd004ed5e3f7b0
Eugene Goya <genygoy@gmail.com>
2019-11-14 15:38:26 +0200

    start created  interface

--- src/main/java/com/netcracker/dao/UserDao.java
+++ src/main/java/com/netcracker/dao/UserDao.java
@@ -1,4 +1,13 @@
 package com.netcracker.dao;
 
+import com.netcracker.models.User;
+
 public interface UserDao {
+    public void createUser();
+    public User getUserById();
+    public User getUserByLogin();
+    public void deleteUser();
+    public void  updateUserPasswordById();
+    public User getUsersByFamilyAccountId();
+    public User getUserByFamilyAccountId();
 }
