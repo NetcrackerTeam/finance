5c7cc1b592987219b992590ee5429fd037ef8244
Dmitriy Ostryanskiy <32409025+summerxwaw@users.noreply.github.com>
2019-11-19 17:56:46 +0200

    Add files via upload

---
+++ src/main/resources/sqlscript/objects_id_s_final.sql
@@ -0,0 +1,18 @@
+BEGIN
+BEGIN
+  EXECUTE IMMEDIATE 'DROP SEQUENCE objects_id_s';
+  EXCEPTION
+  WHEN OTHERS THEN
+  IF SQLCODE != -2289 THEN RAISE;
+  END IF;
+ END;
+BEGIN
+ EXECUTE IMMEDIATE 'create sequence objects_id_s
+  increment by 1
+  start with 1
+  nocache 
+  nocycle';
+END;
+END;
+
+
