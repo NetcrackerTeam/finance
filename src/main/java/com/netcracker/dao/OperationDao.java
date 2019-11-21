156d1e472fb3dcf3d720daf19e6788dab5f64087
killthis <kiillthis@gmail.com>
2019-11-21 20:57:48 +0200

    Merge branch 'master' of /Users/killthis/IdeaProjects/finance with conflicts.

--- src/main/java/com/netcracker/dao/OperationDao.java
+++ src/main/java/com/netcracker/dao/OperationDao.java
@@ -6,23 +6,48 @@ import java.math.BigInteger;
 import java.util.Date;
 
 public interface OperationDao {
-    public void addIncomePersonalByAccId(BigInteger id, int income);
 
-    public void addExpencePersonaByAccId(BigInteger id, int expense);
+    String ADD_INCOME_PERSONAL_BY_ACCOUNT_ID = "INSERT INTO objects(OBJECT_ID, PARENT_ID, OBJECT_TYPE_ID, NAME, DESCRIPTION) " +
+            "VALUES(objects_id_s.nextval, NULL, , '' , '');" +
+            "INSERT INTO objreference(ATTR_ID,OBJECT_ID,REFERENCE) VALUES (?, ? ,?) ";
+    String ADD_EXPENSE_PERSONAL_BY_ACCOUNT_ID = "INSERT INTO objects(OBJECT_ID, PARENT_ID, OBJECT_TYPE_ID, NAME, DESCRIPTION) " +
+            "VALUES(objects_id_s.nextval, NULL, , ? , ?);" +
+            "INSERT INTO objreference(ATTR_ID,OBJECT_ID,REFERENCE) VALUES (?, ? ,?); ";
 
-    public void addIncomeFamilyByAccId(BigInteger id, int income);
+    String ADD_INCOME_FAMILY_BY_ACCOUNT_ID = "INSERT INTO objects(OBJECT_ID, PARENT_ID, OBJECT_TYPE_ID, NAME, DESCRIPTION) " +
+            "VALUES(objects_id_s.nextval, NULL, , ? , ?);" +
+            "INSERT INTO objreference(ATTR_ID,OBJECT_ID,REFERENCE) VALUES (?, ? ,?); ";
+    String ADD_EXPENSE_FAMILY_BY_ACCOUNT_ID = "INSERT INTO objects(OBJECT_ID, PARENT_ID, OBJECT_TYPE_ID, NAME, DESCRIPTION) " +
+            "VALUES(objects_id_s.nextval, NULL, , ? , ?);" +
+            "INSERT INTO objreference(ATTR_ID,OBJECT_ID,REFERENCE) VALUES (?, ? ,?); ";
 
-    public void addExpenseFamilyByAccId(BigInteger id, int expense);
+    String GET_PERSONAL_INCOME_OPERATION_BY_ID_DATE = "Select ";
+    String GET_PERSONAL_EXPENSE_OPERATION_BY_ID_DATE = null;
 
-    public void getPersonalIncomeOperationsByIdDate(BigInteger id, Date date);
+    String GET_FAMILY_INCOME_OPERATION_BY_ID_DATE = null;
+    String GET_FAMILY_EXPENSE_OPERATION_BY_ID_DATE = null;
 
-    public void getPersonalExpenseOperationsByIdDate(BigInteger id, Date date);
+    String GET_CATEGORIES_INCOME = null;
+    String GET_CATEGORIES_EXPENSE = null;
 
-    public void getFamilyIncomeOperationsByIdDate(BigInteger id, Date data);
 
-    public void getFamilyExpenseOperationsByIdDate(BigInteger id, Date data);
+    void addIncomePersonalByAccId(BigInteger id, int income);
 
-    public CategoryIncome getCategoriesIncome();
+    void addExpensePersonaByAccId(BigInteger id, int expense);
 
-    public CategoryExpense getCategoriesExpense();
+    void addIncomeFamilyByAccId(BigInteger id, int income);
+
+    void addExpenseFamilyByAccId(BigInteger id, int expense);
+
+    void getPersonalIncomeOperationsByIdDate(BigInteger id, Date date);
+
+    void getPersonalExpenseOperationsByIdDate(BigInteger id, Date date);
+
+    void getFamilyIncomeOperationsByIdDate(BigInteger id, Date data);
+
+    void getFamilyExpenseOperationsByIdDate(BigInteger id, Date data);
+
+    CategoryIncome getCategoriesIncome();
+
+    CategoryExpense getCategoriesExpense();
 }
