95f3d1e2ca84c71b99ac0dabe585816c6616f80c
Katerina <malutina.catya14@gmail.com>
2019-11-25 14:11:20 +0200

    [OD19DEVAT1-47] Enums changes

---
+++ src/main/java/com/netcracker/models/enums/ReportCategoryExpense.java
@@ -0,0 +1,30 @@
+package com.netcracker.models.enums;
+
+import java.math.BigInteger;
+
+public enum ReportCategoryExpense {
+    DEFAULT(new BigInteger("0")),
+    FOOD(new BigInteger("19")),
+    RESIDENTIAL(new BigInteger("20")),
+    ENTERTAINMENT(new BigInteger("21")),
+    TAXES(new BigInteger("22")),
+    EDUCATION(new BigInteger("23")),
+    MEDICINE(new BigInteger("24")),
+    TRANSPORT(new BigInteger("25")),
+    GIFTS(new BigInteger("26")),
+    CHILDREN(new BigInteger("27")),
+    SPORT(new BigInteger("28")),
+    CLOTHES(new BigInteger("29")),
+    CREDIT(new BigInteger("30")),
+    OTHER(new BigInteger("31"));
+
+    private BigInteger id;
+
+    ReportCategoryExpense(BigInteger bigIntegerId) {
+        this.id = bigIntegerId;
+    }
+
+    public BigInteger getId() {
+        return id;
+    }
+}
---
+++ src/main/java/com/netcracker/models/enums/ReportCategoryIncome.java
@@ -0,0 +1,22 @@
+package com.netcracker.models.enums;
+
+import java.math.BigInteger;
+
+public enum ReportCategoryIncome {
+    DEFAULT(new BigInteger("0")),
+    SALARY(new BigInteger("32")),
+    AWARD(new BigInteger("33")),
+    PERSENTS(new BigInteger("34")),
+    GIFTS(new BigInteger("35")),
+    OTHER(new BigInteger("36"));
+
+    private BigInteger id;
+
+    ReportCategoryIncome(BigInteger bigIntegerId) {
+        this.id = bigIntegerId;
+    }
+
+    public BigInteger getId() {
+        return id;
+    }
+}
