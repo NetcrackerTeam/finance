7bd36ef0fe5bd5483e51a1bb677f6040cb6f29b1
Katerina <malutina.catya14@gmail.com>
2019-11-25 19:45:54 +0200

    [OD19DEVAT1-47] Fix

--- src/main/java/com/netcracker/models/FamilyCreditAccount.java
+++ src/main/java/com/netcracker/models/FamilyCreditAccount.java
@@ -1,4 +1,21 @@
 package com.netcracker.models;
 
 public class FamilyCreditAccount extends AbstractCreditAccount {
+    public static class Builder extends BaseBuilder<AbstractCreditAccount, Builder>{
+
+        public Builder anotherField(String anotherField) {
+            // actualClass.setAnotherField(anotherField);
+            return this;
+        }
+
+        @Override
+        protected FamilyCreditAccount getActual() {
+            return new FamilyCreditAccount();
+        }
+
+        @Override
+        protected Builder getActualBuilder() {
+            return this;
+        }
+    }
 }
