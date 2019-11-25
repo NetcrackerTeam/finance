5f01c01fda90c5d70a1681a85ac1ac016717176b
Sibertooz <sibertooz0sabertooth@aol.com>
2019-11-25 19:10:49 +0200

    AutoOperationDao

--- src/main/java/com/netcracker/dao/AutoOperationDao.java
+++ src/main/java/com/netcracker/dao/AutoOperationDao.java
@@ -1,11 +1,21 @@
 package com.netcracker.dao;
 
-import com.netcracker.models.CategoryIncome;
+import com.netcracker.models.AutoOperationExpense;
+import com.netcracker.models.AutoOperationIncome;
+import com.netcracker.models.enums.CategoryIncome;
 import com.netcracker.models.enums.CategoryExpense;
 
 import java.math.BigInteger;
 
 public interface AutoOperationDao {
+    void getFamilyIncomeAutoOperation(AutoOperationIncome familyIncome);
+
+    void getFamilyExpenseAutoOperation(AutoOperationExpense familyExpense);
+
+    void getPersonalIncomeAutoOperation(AutoOperationIncome personalIncome);
+
+    void getPersonalExpenseAutoOperation(AutoOperationExpense personalExpense);
+
     void createFamilyIncomeAutoOperation(CategoryIncome income);
 
     void createPersonalIncomeAutoOperation(CategoryIncome income );
--- src/main/java/com/netcracker/dao/impl/AutoOperationDaoImpl.java
+++ src/main/java/com/netcracker/dao/impl/AutoOperationDaoImpl.java
@@ -1,12 +1,34 @@
 package com.netcracker.dao.impl;
 
 import com.netcracker.dao.AutoOperationDao;
-import com.netcracker.models.CategoryIncome;
+import com.netcracker.models.AutoOperationExpense;
+import com.netcracker.models.AutoOperationIncome;
+import com.netcracker.models.enums.CategoryIncome;
 import com.netcracker.models.enums.CategoryExpense;
 
 import java.math.BigInteger;
 
 public class AutoOperationDaoImpl implements AutoOperationDao {
+
+    @Override
+    public void getFamilyIncomeAutoOperation(AutoOperationIncome familyIncome) {
+
+    }
+
+    @Override
+    public void getFamilyExpenseAutoOperation(AutoOperationExpense familyExpense) {
+
+    }
+
+    @Override
+    public void getPersonalIncomeAutoOperation(AutoOperationIncome personalIncome) {
+
+    }
+
+    @Override
+    public void getPersonalExpenseAutoOperation(AutoOperationExpense personalExpense) {
+
+    }
 
     @Override
     public void createFamilyIncomeAutoOperation(CategoryIncome income) {
