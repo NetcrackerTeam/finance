594efca6e5dbea618b7e636b0af3b806a6ae6f3c
Katerina <malutina.catya14@gmail.com>
2019-11-29 16:26:19 +0200

    [OD19DEVAT1-51] Fix

--- src/main/java/com/netcracker/HwController.java
+++ src/main/java/com/netcracker/HwController.java
@@ -1,29 +1,14 @@
 package com.netcracker;
 
-import com.netcracker.dao.FamilyAccountDebitDao;
-import com.netcracker.dao.PersonalDebitAccountDao;
-import com.netcracker.dao.impl.FamilyAccountDebitDaoImpl;
-import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.math.BigInteger;
-import java.util.Locale;
-
 @Controller
 public class HwController {
-@Autowired
-private FamilyAccountDebitDao familyAccountDebitDao;
-//@Autowired
-//private PersonalDebitAccountDao personalDebitAccountDao;
-    Logger logger = Logger.getLogger(HwController.class);
-    @GetMapping( "/" )
-    public String welcomePage(Model model) {
 
-        Locale.setDefault(Locale.ENGLISH);
-     //   logger.debug(personalDebitAccountDao.getPersonalAccountById(BigInteger.valueOf(2)).getObjectName());
+    @GetMapping("/")
+    public String welcomePage(Model model) {
         model.addAttribute("hello", "HELLO WORLD");
         return "index";
     }
