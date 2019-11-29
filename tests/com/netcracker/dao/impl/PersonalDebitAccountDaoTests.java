a592ee9677592e016875da72de1ffa8314eefc35
Dmitriy <Summerxwow@gmail.com>
2019-11-29 14:56:27 +0200

    OD19DEVAT1-48

--- tests/com/netcracker/dao/impl/PersonalDebitAccountDaoTests.java
+++ tests/com/netcracker/dao/impl/PersonalDebitAccountDaoTests.java
@@ -1,26 +1,31 @@
 package com.netcracker.dao.impl;
 
+import com.netcracker.configs.WebConfig;
 import com.netcracker.dao.PersonalDebitAccountDao;
+import com.netcracker.dao.UserDao;
 import com.netcracker.models.PersonalDebitAccount;
+import com.netcracker.models.User;
 import com.netcracker.models.enums.PersonalAccountStatusActive;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
 
 import javax.sql.DataSource;
 import java.math.BigInteger;
 import java.util.Locale;
 
-@TestExecutionListeners( {DependencyInjectionTestExecutionListener.class })
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:Spring-Config.xml"})
+@WebAppConfiguration
+@ContextConfiguration(classes = WebConfig.class)
 public class PersonalDebitAccountDaoTests {
 
     @Autowired
@@ -29,7 +34,17 @@ public class PersonalDebitAccountDaoTests {
     private DataSource dataSource;
     private JdbcTemplate template;
     private PersonalDebitAccount personalDebitAccount;
-
+    private static final String  CREATE_USER = "INSERT ALL " +
+            "INTO OBJECTS(OBJECT_ID,OBJECT_TYPE_ID,NAME) VALUES (127, 1, 'user_Tests') "
+            +
+            "INTO ATTRIBUTES(ATTR_ID, OBJECT_ID, VALUE) VALUES(5, 127, 'UserTests') "
+            +
+            "INTO ATTRIBUTES(ATTR_ID, OBJECT_ID, VALUE) VALUES(3, 127, 'mail@gmail.com') "
+            +
+            "INTO ATTRIBUTES(ATTR_ID, OBJECT_ID, VALUE) VALUES(4, 127, 'password') "
+            +
+            "SELECT * " +
+                    "FROM Dual";
 
     @Before
     public void setUp() {
@@ -39,16 +54,49 @@ public class PersonalDebitAccountDaoTests {
 
     @Test
     public void getPersonalAccountById() {
-        PersonalDebitAccount personalDebitAccount = personalDebitAccountDao.getPersonalAccountById(BigInteger.valueOf(1));
+        BigInteger id = BigInteger.valueOf(25);
+        PersonalDebitAccount personalDebitAccount =
+                personalDebitAccountDao.getPersonalAccountById(id);
+        BigInteger ex_id = id;
+        String name = "PER_DEB_ACC2";
+        Long amount = 10000L;
+        assertEquals(ex_id,personalDebitAccount.getId());
+        assertEquals(name, personalDebitAccount.getObjectName());
+        assertEquals(amount, personalDebitAccount.getAmount());
+
+        assertEquals(BigInteger.valueOf(24), personalDebitAccount.getOwner().getId());
+        assertEquals("Dimas", personalDebitAccount.getOwner().getName());
+        assertEquals("mailDimas@gmail.com", personalDebitAccount.getOwner().geteMail());
+        assertEquals("passwordDima", personalDebitAccount.getOwner().getPassword());
     }
     @Test
     public void createPersonalAccount(){
-        personalDebitAccount = new PersonalDebitAccount.Builder()
-                .debitId(BigInteger.valueOf(101))
-                .debitObjectName("test")
-                .debitAmount(Long.valueOf(1234))
-                .debitPersonalAccountStatus(PersonalAccountStatusActive.getStatusByKey(BigInteger.valueOf(43)))
+        User owner = new User.Builder()
+                .user_id(BigInteger.valueOf(125))
+                .user_name("Dimas")
+                .user_eMail("mailDimas@gmail.com")
+                .user_password("passwordDima")
                 .build();
-        personalDebitAccountDao.createPersonalAccount(personalDebitAccount);
+        template.update(CREATE_USER);
+
+        PersonalDebitAccount personalDebitAccount = new PersonalDebitAccount.Builder()
+                .debitObjectName("PER_DEB_ACC1")
+                .debitAmount(Long.valueOf(1234L))
+                .debitPersonalAccountStatus(PersonalAccountStatusActive.YES)
+                .debitOwner(owner)
+                .build();
+
+        PersonalDebitAccount personalDebitAccount1 = personalDebitAccountDao.createPersonalAccount(personalDebitAccount);
+        assertEquals("PER_DEB_ACC1",personalDebitAccount1.getObjectName());
+    }
+
+    @Test
+    public void  deletePersonalAccountById (){
+        personalDebitAccountDao.deletePersonalAccountById(BigInteger.valueOf(121), BigInteger.valueOf(43));
+    }
+
+    @Test
+    public void deletePersonalAccountByUserId (){
+        personalDebitAccountDao.deletePersonalAccountByUserId(BigInteger.valueOf(121));
     }
 }
