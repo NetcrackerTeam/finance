601e2fe00a9cab68a27d6b5e8117238339022ff8
Eugene Goya <genygoy@gmail.com>
2019-12-06 15:37:04 +0200

    fix

--- src/main/java/com/netcracker/services/impl/MonthReportServiceImpl.java
+++ src/main/java/com/netcracker/services/impl/MonthReportServiceImpl.java
@@ -1,13 +1,25 @@
 package com.netcracker.services.impl;
 
+import com.netcracker.models.MonthReport;
 import com.netcracker.services.MonthReportService;
 
+import java.math.BigInteger;
 import java.time.LocalDate;
 
 public class MonthReportServiceImpl implements MonthReportService {
 
+//    @Override
+//    public void formMonthReportFromDb(LocalDate date) {
+//
+//    }
+
     @Override
-    public void formMonthReportFromDb(LocalDate date) {
+    public void formMonthPersonalReportFromDb(BigInteger id, LocalDate date) {
+
+    }
+
+    @Override
+    public void formMonthFamilyReportFromDb(BigInteger id, LocalDate date) {
 
     }
 
@@ -17,7 +29,17 @@ public class MonthReportServiceImpl implements MonthReportService {
     }
 
     @Override
-    public void getMonthReport() {
-
+    public MonthReport getMonthPersonalReport(LocalDate date_from, LocalDate date_to) {
+        return null;
     }
+
+    @Override
+    public MonthReport getMonthFamilyReport(LocalDate date_from, LocalDate date_to) {
+        return null;
+    }
+
+//    @Override
+//    public void getMonthReport() {
+//
+//    }
 }
