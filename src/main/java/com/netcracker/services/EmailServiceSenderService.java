fbde3ff556d6f7041fe4861c21305af35ebf5615
Dmitriy Ostryanskiy <32409025+summerxwaw@users.noreply.github.com>
2019-12-09 15:36:37 +0200

    fixMailSender

--- src/main/java/com/netcracker/services/EmailServiceSenderService.java
+++ src/main/java/com/netcracker/services/EmailServiceSenderService.java
@@ -3,9 +3,9 @@ package com.netcracker.services;
 import com.netcracker.models.AccountExpense;
 import com.netcracker.models.User;
 
-import java.math.BigInteger;
 
 public interface EmailServiceSenderService {
+
     public void sendMailBeforeDeactivate(User user);
 
     public void sendMailAboutDebt(String emailAddressTo, String login, String password);
@@ -15,4 +15,6 @@ public interface EmailServiceSenderService {
     public void sendMailReminderExpense(String emailAddressTo, String login, AccountExpense accountExpense);
 
     public void sendMailAutoIncome(String emailAddressTo, String login, String password);
+
+    String DEACTIVATE_ACCOUNT = "SELECT NAME, MESSAGE FROM TEMPLATE WHERE ID = 9";
 }
