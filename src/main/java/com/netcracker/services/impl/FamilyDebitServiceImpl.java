46e78c701a1f0b37d82123ffc47244766c04d429
Jenja Makovey <jenjamak@gmail.com>
2019-12-09 15:18:35 +0200

    makovey family account

--- src/main/java/com/netcracker/services/impl/FamilyDebitServiceImpl.java
+++ src/main/java/com/netcracker/services/impl/FamilyDebitServiceImpl.java
@@ -1,6 +1,8 @@
 package com.netcracker.services.impl;
 
-import com.netcracker.dao.*;
+import com.netcracker.dao.FamilyAccountDebitDao;
+import com.netcracker.dao.OperationDao;
+import com.netcracker.dao.UserDao;
 import com.netcracker.dao.impl.FamilyAccountDebitDaoImpl;
 import com.netcracker.models.*;
 import com.netcracker.models.enums.UserStatusActive;
@@ -9,7 +11,9 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.math.BigInteger;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
 
 public class FamilyDebitServiceImpl implements FamilyDebitService {
 
@@ -24,58 +28,59 @@ public class FamilyDebitServiceImpl implements FamilyDebitService {
 
     @Override
     public FamilyDebitAccount createFamilyDebitAccount(FamilyDebitAccount familyDebitAccount) {
+        logger.debug("Entering insert(createFamilyDebitAccount=" + familyDebitAccount + ")");
         return familyAccountDebitDao.createFamilyAccount(familyDebitAccount);
     }
 
     @Override
     public FamilyDebitAccount getFamilyDebitAccount(BigInteger id) {
+        logger.debug("Entering select(getFamilyDebitAccount=" + id + ")");
         return familyAccountDebitDao.getFamilyAccountById(id);
     }
 
     @Override
     public void deleteFamilyDebitAccount(BigInteger id) {
+        logger.debug("Entering update(deleteFamilyDebitAccount=" + id + ")");
         familyAccountDebitDao.deleteFamilyAccount(id);
     }
 
     @Override
     public void addUserToAccount(BigInteger accountId, BigInteger userId) {
+        logger.debug("Entering insert(addUserToAccount=" + accountId + " " + userId + ")");
         try {
             User tempUser = userDao.getUserById(userId);
-            if(tempUser == null){
-                throw new NullPointerException("User " + userId + " is NULL");
-            }
-            if (tempUser.getUserStatusActive() == UserStatusActive.NO ) {
+            if (tempUser.getUserStatusActive() == UserStatusActive.NO) {
                 logger.error("The user " + userId + " is unActive");
             } else {
-                Collection<User> partisipants = familyAccountDebitDao.getParticipantsOfFamilyAccount(accountId);
-                if(partisipants == null){
-                    throw new NullPointerException("Partisipants of family debit account by id " + accountId + " is NULL");
-                }
-                for (User participant : partisipants) {
+                Collection<User> participants = familyAccountDebitDao.getParticipantsOfFamilyAccount(accountId);
+                for (User participant : participants) {
                     if (participant.getId().equals(userId)) {
                         logger.error("The user " + participant.getId() + " is has family account");
                     }
                 }
                 familyAccountDebitDao.addUserToAccountById(accountId, userId);
+                logger.debug("Entering insert success(addUserToAccount=" + accountId + " " + userId + ")");
             }
-        }
-        catch (NullPointerException e){
-                logger.error(e.getMessage() , e);
+        } catch (NullPointerException e) {
+            logger.error(e.getMessage(), e);
         }
     }
 
     @Override
     public void deleteUserFromAccount(BigInteger accountId, BigInteger userId) {
+        logger.debug("Entering update(deleteUserFromAccount=" + accountId + " " + userId + ")");
         familyAccountDebitDao.deleteUserFromAccountById(accountId, userId);
     }
 
     @Override
     public Collection<AbstractAccountOperation> getHistory(BigInteger familyAccountId, Date date) {
+        logger.debug("Entering select(getHistory=" + familyAccountId + " " + date + ")");
         Collection<AbstractAccountOperation> objects = new ArrayList<>();
         Collection<AccountIncome> incomes = operationDao.getIncomesFamilyAfterDateByAccountId(familyAccountId, date);
         Collection<AccountExpense> expenses = operationDao.getExpensesFamilyAfterDateByAccountId(familyAccountId, date);
         objects.addAll(incomes);
         objects.addAll(expenses);
+        logger.debug("Entering select success(getHistory=" + familyAccountId + " " + date + ")");
         return objects;
     }
 }
