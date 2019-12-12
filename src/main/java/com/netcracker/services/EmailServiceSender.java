94312e1c68ade6bcdc3c77127e1cbce33d43c65c
Dmitriy Ostryanskiy <32409025+summerxwaw@users.noreply.github.com>
2019-12-12 16:28:04 +0200

    OD19DEVAT1-76-changedInterface

---
+++ src/main/java/com/netcracker/services/EmailServiceSender.java
@@ -0,0 +1,32 @@
+package com.netcracker.services;
+
+import com.netcracker.models.User;
+
+import javax.swing.plaf.basic.BasicButtonUI;
+import java.math.BigInteger;
+import java.util.Date;
+
+
+public interface EmailServiceSender {
+
+    public void sendMailBeforeDeactivate(String emailTo, String userName, BigInteger userId);
+
+    public void sendMailAboutPersonalDebt(String emailTo, String userName,String perName, BigInteger userId);
+
+    public void sendMailAboutFamilyDebt(String emailTo, String userName,String famName, BigInteger userId);
+
+    public void sendMailReminderPersonalCredit(User user, BigInteger id, BigInteger userId, Date date);
+
+    public void sendMailReminderFamilyCredit(User user, BigInteger id, BigInteger userId, Date date);
+
+    public void sendMailReminderPersonalExpense(User user, BigInteger id, BigInteger userId, Date date);
+
+    public void sendMailAutoPersonalIncome(User user, BigInteger id, BigInteger userId, Date date);
+
+    public void sendMailReminderFamilyExpense(User user, BigInteger id, BigInteger userId, Date date);
+
+    public void sendMailAutoFamilyIncome(User user, BigInteger id, BigInteger userId, Date date);
+
+
+
+}
