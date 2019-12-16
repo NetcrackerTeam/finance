5f0787e37d9b4b173191b1066dd99f44f9c55f98
Eugene Goya <genygoy@gmail.com>
2019-12-16 18:16:56 +0200

    [OD19DEVAT1] Job Service

--- src/main/java/com/netcracker/services/JobService.java
+++ src/main/java/com/netcracker/services/JobService.java
@@ -2,22 +2,24 @@ package com.netcracker.services;
 
 
 public interface JobService {
+    String CRON_BY_EVERYDAY = "0 0 8 * * ?";//at 8 am every day
+
+    String CRON_BY_REPORT = "0 0 18 L * ?";//At 6 pm on the last of every month
 
     public void executeMonthPersonalReportOnEmailJob();
 
     public void executeMonthFamilyReportOnEmailJob();
 
-    public void executeRemindAutoIncomePersonalEmailJob();
+    public void executeRemindAutoIncomeFamilyJob();
 
-    public void executeRemindAutoExpenseFamilyEmailJob();
+    public void executeRemindAutoExpenseFamilyJob();
 
-    public void  executeRemindAutoIncomeFamilyEmailJob();
+    public void executeRemindAutoExpensePersonalJob();
 
-    public void executeRemindAutoExpensePersonalEmailJob();
+    public void executeRemindAutoIncomePersonalJob();
 
-    public  void  executePersonalAutoDebtRepayment();
+    public void executeAutoCreditExpenseFamily();
 
-    public void executeFamilyAutoDebtRepayment();
-
+    public void executeAutoCreditExpensePersonal();
 
 }
--- src/main/java/com/netcracker/services/impl/JobServiceImpl.java
+++ src/main/java/com/netcracker/services/impl/JobServiceImpl.java
@@ -1,14 +1,21 @@
 package com.netcracker.services.impl;
 
+import com.netcracker.dao.CreditAccountDao;
+import com.netcracker.exception.JobException;
+import com.netcracker.models.*;
 import com.netcracker.services.*;
+import com.netcracker.services.utils.CreditUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import javax.mail.MessagingException;
 import java.nio.file.Path;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.Collection;
+import java.util.List;
 
 @Service
 public class JobServiceImpl implements JobService {
@@ -19,12 +26,13 @@ public class JobServiceImpl implements JobService {
     private MonthReportService monthReportService;
 
     @Autowired
-    private AccountAutoOperationService accountAutoOperationService;
+    private CreditAccountDao creditAccountDao;
 
+    @Autowired
+    private AccountAutoOperationService accountAutoOperationService;
 
     @Autowired
     private EmailServiceSender emailServiceSender;
-
 
     @Autowired
     private FamilyCreditService familyCreditService;
@@ -32,106 +40,142 @@ public class JobServiceImpl implements JobService {
     @Autowired
     private PersonalCreditService personalCreditService;
 
+    @Autowired
+    private FamilyDebitService familyDebitService;
+
+    @Autowired
+    private OperationService operationService;
+
+    private LocalDate localDateTo = LocalDate.now();
+    private LocalDate dateFrom = localDateTo.minus(1, ChronoUnit.MONTHS);
+    private int dayNow = localDateTo.getDayOfMonth();
     private Path path;
-    private static final String CRON_BY_EVERYDAY = "0 0 8 * * ?";//at 8 am every day
-    private static final String CRON_BY_REPORT = "0 0 18 L * ?";//At 6 pm on the last of every month
-    private LocalDate dateTo = LocalDate.now();
-    private LocalDate dateFrom = dateTo.minus(1, ChronoUnit.MONTHS);
-
-    @Override
-    @Scheduled(cron = CRON_BY_EVERYDAY)
-    public void executeRemindAutoIncomePersonalEmailJob() {
-//          Collection<AutoOperationIncome> operationIncomeList =
-//                  (Collection<AutoOperationIncome>) accountAutoOperationService.getFamilyIncomeAutoOperation(monthId);
-//          if (!operationIncomeList.isEmpty()){
-//              operationIncomeList.forEach(operationIncome->{
-//                  try {
-//                      emailServiceSender.sendMailAutoFamilyIncome(user.geteMail(),user.getName(),operationIncome.getAmount(),
-//                              String.valueOf(operationIncome.getCategoryIncome()),user.getId());
-//                      logger.debug("Email have been sent.");
-//                  }catch (JobException e){
-//                      logger.debug("Email can't be sent", e);
-//                  }
-//              });
-//          }
-    }
-
-    @Override
-    @Scheduled(cron = CRON_BY_EVERYDAY)
-    public void executeRemindAutoExpenseFamilyEmailJob() {
-//        Collection<AutoOperationExpense> operationExpensesList =
-//                (Collection<AutoOperationExpense>) accountAutoOperationService.getFamilyExpenseAutoOperation(monthId);
-//        if (!operationExpensesList.isEmpty()){
-//            operationExpensesList.forEach(operationExpense->{
-//                try {
-//                    emailServiceSender.sendMailAutoFamilyIncome(user.geteMail(),user.getName(),operationExpense.getAmount(),
-//                            String.valueOf(operationExpense.getCategoryExpense()),user.getId());
-//                    logger.debug("Email have been sent.");
-//                }catch (JobException e){
-//                    logger.debug("Email can't be sent", e);
-//                }
-//            });
-//        }
-    }
+    double calculateCredit;
+    private List<User> userList;
 
     @Override
     @Scheduled(cron = CRON_BY_REPORT)
     public void executeMonthFamilyReportOnEmailJob() {
-//        Collection<MonthReport> monthReportsFamily =
-//                (List<MonthReport>) monthReportService.getMonthPersonalReport(monthId, dateFrom, dateTo);
-//        if (!monthReportsFamily.isEmpty()) {
-//            monthReportsFamily.forEach(monthReport -> {
-//                try {
-//                    path = monthReportService.convertToTxt(monthReport);
-//                    //  emailServiceSender.sendMailAboutFamilyDebt(path);
-//                    logger.debug("Email have been sent. User id: {}, Date: {}"+ user.getId());
-//                } catch (JobException e) {
-//                    logger.debug("Email can't be sent", e);
-//                }
-//            });
-//        }
+        Collection<FamilyDebitAccount> familyDebitAccounts = familyDebitService.getAllFamilyAccounts();
+        if (!familyDebitAccounts.isEmpty()) {
+            familyDebitAccounts.forEach(debitAccountFamily -> {
+                userList = debitAccountFamily.getParticipants();
+                for (User user : userList) {
+                    Collection<MonthReport> monthReportsFamily =
+                            (List<MonthReport>) monthReportService.getMonthPersonalReport(user.getFamilyDebitAccount(), dateFrom, localDateTo);
+                    if (!monthReportsFamily.isEmpty()) {
+                        monthReportsFamily.forEach(monthReport -> {
+                            try {
+                                path = monthReportService.convertToTxt(monthReport);
+                                emailServiceSender.monthReport(user.geteMail(), user.getId());// не дописан sender не могу передать путь к файлу
+                                logger.debug("Email have been sent. User id: {}, Date: {}" + user.getId());
+                            } catch (JobException e) {
+                                logger.debug("Email can't be sent", e);
+                            } catch (MessagingException e) {
+                                logger.debug("Email can't be sent, messaging exception", e);
+                            }
+                        });
+                    }
+                }
+            });
+        }
     }
 
     @Override
     @Scheduled(cron = CRON_BY_REPORT)
     public void executeMonthPersonalReportOnEmailJob() {
-//        Collection<MonthReport> monthReportsFamily =
-//                (List<MonthReport>) monthReportService.getMonthFamilyReport(monthId, dateFrom, dateTo);
-//        if (!monthReportsFamily.isEmpty()) {
-//            monthReportsFamily.forEach(monthReport -> {
-//                try {
-//                    path = monthReportService.convertToTxt(monthReport);
-//                   // emailServiceSender.monthReport(path);
-//                    logger.debug("Email have been sent. User id: {}" + user.getId());
-//                } catch (JobException e) {
-//                    logger.debug("Email can't be sent", e);
-//                }
-//            });
-//        }
-    }
+        Collection<FamilyDebitAccount> familyDebitAccounts = familyDebitService.getAllFamilyAccounts();
+        if (!familyDebitAccounts.isEmpty()) {
+            familyDebitAccounts.forEach(debitAccountFamily -> {
+                userList = debitAccountFamily.getParticipants();
+                for (User user : userList) {
+                    Collection<MonthReport> monthReportsFamily =
+                            (List<MonthReport>) monthReportService.getMonthFamilyReport(user.getPersonalDebitAccount(), dateFrom, localDateTo);
+                    if (!monthReportsFamily.isEmpty()) {
+                        monthReportsFamily.forEach(monthReport -> {
+                            try {
+                                path = monthReportService.convertToTxt(monthReport);
+                                emailServiceSender.monthReport(user.geteMail(), user.getId());//дописать сендер
+                                logger.debug("Email have been sent. User id: {}" + user.getId());
+                            } catch (JobException e) {
+                                logger.debug("Email can't be sent", e);
+                            } catch (MessagingException e) {
+                                logger.debug("Email can't be sent, messaging exception", e);
 
-
-    @Override
-    @Scheduled(cron = CRON_BY_EVERYDAY)
-    public void executeRemindAutoIncomeFamilyEmailJob() {
-
-    }
-
-    @Override
-    @Scheduled(cron = CRON_BY_EVERYDAY)
-    public void executeRemindAutoExpensePersonalEmailJob() {
-
-    }
-
-    @Override
-    @Scheduled(cron = CRON_BY_EVERYDAY)
-    public void executePersonalAutoDebtRepayment() {
-
+                            }
+                        });
+                    }
+                }
+            });
+        }
     }
 
     @Override
     @Scheduled(cron = CRON_BY_EVERYDAY)
-    public void executeFamilyAutoDebtRepayment() {
+    public void executeRemindAutoIncomeFamilyJob() {
+        Collection<AutoOperationIncome> autoOperationIncomesFamily = accountAutoOperationService.getAllTodayOperationsFamilyIncome(dayNow);
+        for (AutoOperationIncome autoIncome: autoOperationIncomesFamily) {
+            operationService.createFamilyOperationIncome(autoIncome.getDebitId(),autoIncome.getDebitId(),autoIncome.getAmount() ,localDateTo,autoIncome.getCategoryIncome());
+            familyCreditService.getFamilyCredits(autoIncome.getDebitId());
 
+        }
+    }
+
+    @Override
+    @Scheduled(cron = CRON_BY_EVERYDAY)
+    public void executeRemindAutoExpenseFamilyJob() {
+        Collection<AutoOperationExpense> autoOperationExpense = accountAutoOperationService.getAllTodayOperationsFamilyExpense(dayNow);
+        for (AutoOperationExpense autoExpense: autoOperationExpense) {
+            operationService.createFamilyOperationExpense(autoExpense.getId(),autoExpense.getUserId(),autoExpense.getAmount(),localDateTo,autoExpense.getCategoryExpense());
+            familyCreditService.getFamilyCredits(autoExpense.getId());
+        }
+
+    }
+
+    @Override
+    @Scheduled(cron = CRON_BY_EVERYDAY)
+    public void executeRemindAutoExpensePersonalJob() {
+        Collection<AutoOperationExpense> autoOperationExpensePersonal = accountAutoOperationService.getAllTodayOperationsPersonalExpense(dayNow);
+        for (AutoOperationExpense autoExpense: autoOperationExpensePersonal) {
+            operationService.createPersonalOperationExpense(autoExpense.getId(),autoExpense.getAmount(),localDateTo,autoExpense.getCategoryExpense());
+            personalCreditService.getPersonalCredits(autoExpense.getId());
+        }
+    }
+
+    @Override
+    @Scheduled(cron = CRON_BY_EVERYDAY)
+    public void executeRemindAutoIncomePersonalJob() {
+        Collection<AutoOperationIncome> autoOperationIncomePersonal = accountAutoOperationService.getAllTodayOperationsPersonalIncome(dayNow);
+        for (AutoOperationIncome autoIncome: autoOperationIncomePersonal) {
+            operationService.createPersonalOperationIncome(autoIncome.getDebitId(),autoIncome.getAmount(), localDateTo,autoIncome.getCategoryIncome());
+            personalCreditService.getPersonalCredits(autoIncome.getDebitId());
+        }
+    }
+// проверить кредитный счет
+
+    @Override
+    @Scheduled(cron = CRON_BY_EVERYDAY)
+    public void executeAutoCreditExpenseFamily() {
+        Collection<FamilyCreditAccount> allFamilyCredit = creditAccountDao.getAllFamilyCreditIdsByMonthDay(dayNow);
+        for (FamilyCreditAccount familyCredit : allFamilyCredit) {
+            calculateCredit = CreditUtils.calculateMonthPayment(dateFrom, localDateTo, familyCredit.getPaidAmount(), familyCredit.getCreditRate());
+            boolean paymentAutoFamilyCredit = personalCreditService.addPersonalCreditPaymentAuto(familyCredit.getDebt().getDebtId(), familyCredit.getCreditId(), calculateCredit);
+            if (!paymentAutoFamilyCredit) {
+                personalCreditService.increaseDebt(familyCredit.getCreditId(), familyCredit.getPaidAmount());
+            }
+        }
+    }
+
+    @Override
+    @Scheduled(cron = CRON_BY_EVERYDAY)
+    public void executeAutoCreditExpensePersonal() {
+        Collection<PersonalCreditAccount> allPersonalCredit = creditAccountDao.getAllPersonCreditIdsByMonthDay(dayNow);
+        for (PersonalCreditAccount personalCredit : allPersonalCredit) {
+            calculateCredit = CreditUtils.calculateMonthPayment(dateFrom, localDateTo, personalCredit.getPaidAmount(), personalCredit.getCreditRate());
+            boolean paymentAutoPersonalCredit = familyCreditService.addFamilyCreditPaymentAuto(personalCredit.getDebt().getDebtId(), personalCredit.getCreditId(), calculateCredit);
+            if (!paymentAutoPersonalCredit) {
+                familyCreditService.increaseDebt(personalCredit.getCreditId(), personalCredit.getPaidAmount());
+            }
+        }
     }
 }
