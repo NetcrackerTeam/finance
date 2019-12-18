c63b3d9b6d9cbe1ff15255e921197471a229ce82
Jenja Makovey <jenjamak@gmail.com>
2019-12-18 20:17:57 +0200

    [OD19DEVAT1-82]

--- src/main/java/com/netcracker/controllers/PersonalDebitController.java
+++ src/main/java/com/netcracker/controllers/PersonalDebitController.java
@@ -1,58 +1,113 @@
 package com.netcracker.controllers;
 
-import com.google.gson.Gson;
-import com.netcracker.models.PersonalCreditAccount;
-import com.netcracker.models.Status;
-import com.netcracker.services.PersonalCreditService;
+import com.netcracker.dao.AutoOperationDao;
+import com.netcracker.dao.PersonalDebitAccountDao;
+import com.netcracker.models.AccountExpense;
+import com.netcracker.models.AutoOperationExpense;
+import com.netcracker.models.User;
+import com.netcracker.models.enums.CategoryExpense;
+import com.netcracker.services.AccountAutoOperationService;
+import com.netcracker.services.UserService;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.math.BigInteger;
+import java.time.LocalDate;
 
 @Controller
+@RequestMapping("/PersonalDebit")
 public class PersonalDebitController {
 
-
     @Autowired
-    PersonalCreditService creditService;
+    PersonalDebitAccountDao personalDebitAccountDao;
+    @Autowired
+    AutoOperationDao autoOperationDao;
+    @Autowired
+    AccountAutoOperationService accountAutoOperationService;
+    @Autowired
+    UserService userService;
 
-    @RequestMapping(value = "{id}/addCredit", method = RequestMethod.POST)
-    public String addCreditAccount(@RequestBody PersonalCreditAccount creditAccount,
-                                   @PathVariable("id") BigInteger id, Model model) {
-        creditService.createPersonalCredit(id, creditAccount);
-        Gson gson = new Gson();
-        Status status = new Status(true, "Credit account was created in account " + id);
-        model.addAttribute("json_res", gson.toJson(status));
-        return "test";
+    private static final Logger logger = Logger.getLogger(PersonalDebitController.class);
+
+    @RequestMapping(value = "/addCreditAcc", method = RequestMethod.POST )
+    public String addCreditAccount(){
+        return null;
     }
 
-    public void addIncomePersonal() {
+    @RequestMapping(value = "/addIncomePersonal", method = RequestMethod.POST )
+    public String addIncomePersonal(
+    ){
+        return null;
     }
 
-    public void addExpensePersonal() {
+    @RequestMapping(value = "/addExpensePersonal", method = RequestMethod.POST )
+    public String addExpensePersonal(){
+        return null;
     }
 
-    public void getHistory() {
+    @RequestMapping(value = "/getHistory", method = RequestMethod.GET )
+    public String getHistory(){
+        return null;
     }
 
-    public void createAutoIncome() {
+    @RequestMapping(value = "/createAutoIncome", method = RequestMethod.POST )
+    public String createAutoIncome(@RequestParam(value = "userId") BigInteger userId,
+                                   @RequestParam(value = "categoryIncome") CategoryExpense categoryIncome,
+                                   @RequestParam(value = "incomeAmount") Double incomeAmount,
+                                   @RequestParam(value = "dateIncome") LocalDate dateincome,
+                                   @RequestParam(value = "dayOfMonth") int dayOfMonth){
+            User user = userService.getUserById(userId);
+            BigInteger personalId = user.getPersonalDebitAccount();
+            AutoOperationExpense autoOperationExpense = new AutoOperationExpense.Builder()
+                    .categoryExpense(categoryIncome)
+                    .accountAmount(incomeAmount)
+                    .accountDate(dateincome)
+                    .dayOfMonth(dayOfMonth)
+                    .build();
+            accountAutoOperationService.createPersonalExpenseAutoOperation(autoOperationExpense, personalId);
+            logger.debug("expense is done!");
+            return "success/autoIncome";
     }
 
-    public void createAutoExpense() {
+    @RequestMapping(value = "/createAutoExpense", method = RequestMethod.POST )
+    public String createAutoExpense(
+            @RequestParam(value = "userId") BigInteger userId,
+            @RequestParam(value = "categoryExpense") CategoryExpense categoryExpense,
+            @RequestParam(value = "expenseAmount") Double expenseAmount,
+            @RequestParam(value = "dateExpense") LocalDate dateExpense,
+            @RequestParam(value = "dayOfMonth") int dayOfMonth
+    ){
+        User user = userService.getUserById(userId);
+        AutoOperationExpense autoOperationExpense = new AutoOperationExpense.Builder()
+                .categoryExpense(categoryExpense)
+                .accountAmount(expenseAmount)
+                .accountDate(dateExpense)
+                .dayOfMonth(dayOfMonth)
+                .build();
+        accountAutoOperationService.createPersonalExpenseAutoOperation(autoOperationExpense, user.getPersonalDebitAccount());
+        logger.debug("expense is done!");
+        return "expensePersonal/ready";
     }
 
-    public void deleteAutoIncome() {
+
+    @RequestMapping(value = "/deleteAutoIncome", method = RequestMethod.POST )
+    public String deleteAutoIncome(){
+        return null;
     }
 
-    public void deleteAutoExpence() {
+    @RequestMapping(value = "/deleteAutoExpense", method = RequestMethod.POST )
+    public String deleteAutoExpense(){
+        return null;
     }
 
-    public void getReport() {
+    @RequestMapping(value = "/getReport", method = RequestMethod.POST )
+    public String getReport(){
+        return null;
     }
 
 }
