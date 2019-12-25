52506bf2115008eddf4e88075b3fa8d4bcf2613a
Dmitriy Ostryanskiy <32409025+summerxwaw@users.noreply.github.com>
2019-12-25 15:46:20 +0200

    [OD19DEVAT1 - 85] - htmlPersonalDebit

--- src/main/webapp/resources/js/controllers/PersonalDebitController.js
+++ src/main/webapp/resources/js/controllers/PersonalDebitController.js
@@ -1,15 +1,21 @@
 'use strict';
 
 /**
- * CarController
+ * PersonalDebitController
  * @constructor
  */
 var PersonalDebitController = function($scope, $http) {
     $scope.fetchPersonalAccount = function() {
-        $http.get('/debitPersonal/layout').success(function(personalAccount){
+        $http.get('debitPersonal/layout').success(function(personalAccount){
             $scope.personalAccount = personalAccount;
+        });
+    };
+    $scope.fetchPersonalHistory = function(){
+        $http.get('debitPersonal/history').success(function(personalHistory) {
+            $scope.personalHistory = personalHistory;
         });
     };
 
     $scope.fetchPersonalAccount();
+    $scope.fetchPersonalHistory();
 };
