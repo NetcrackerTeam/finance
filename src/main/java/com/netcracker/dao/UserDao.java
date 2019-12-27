bef53516bf31a97018083263f0c0875c15659dfe
Eugene Goya <genygoy@gmail.com>
2019-12-27 23:39:42 +0200

    sing up controller

--- src/main/java/com/netcracker/dao/UserDao.java
+++ src/main/java/com/netcracker/dao/UserDao.java
@@ -1,28 +1,27 @@
 package com.netcracker.dao;
 
 import com.netcracker.models.User;
-import com.netcracker.models.enums.UserStatusActive;
 
 import java.math.BigInteger;
 
 public interface UserDao {
 
-    public User createUser(User user);
+    User createUser(User user);
 
 
-    public User getUserById(BigInteger id);
+    User getUserById(BigInteger id);
 
 
-    public User getUserByEmail(String login);
+    User getUserByEmail(String login);
 
 
-    public void updateUserPasswordById(BigInteger id, String newPassword);
+    void updateUserPasswordById(BigInteger id, String newPassword);
 
 
-    public  void  updateUserStatus(BigInteger id , BigInteger newStatus);
+    void updateUserStatus(BigInteger id, BigInteger newStatus);
 
 
-    public  void  updateEmail(BigInteger id , String newEmail) ;
+    void updateEmail(BigInteger id, String newEmail);
 
 
     String CREATE_USER = "INSERT ALL  " +
@@ -34,29 +33,20 @@ public interface UserDao {
             "SELECT * " +
             "FROM Dual";
 
-    String GET_USER_BY_LOGIN = "SELECT EMP.OBJECT_ID AS USER_ID, EMP_NAME.VALUE AS NAME, EMP_EMAIL.VALUE AS EMAIL, " +
-            "PASSWORD.VALUE AS PASSWORD, EMP_STATUS.LIST_VALUE_ID AS IS_ACTIVE, PER.OBJECT_ID AS PER_DEB_ACC1, FAM.OBJECT_ID AS FAM_DEB_ACC1 " +
-            "FROM  OBJECTS EMP, OBJECTS PER, OBJECTS FAM, " +
-            "ATTRIBUTES EMP_NAME, ATTRIBUTES EMP_EMAIL, ATTRIBUTES PASSWORD, ATTRIBUTES EMP_STATUS, " +
-            "OBJREFERENCE PER_US, OBJREFERENCE FAM_US "
-            + "WHERE  EMP.OBJECT_TYPE_ID = 1 " +
-            "AND PER.OBJECT_TYPE_ID = 2" +
-            " AND FAM.OBJECT_TYPE_ID = 13  " +
-            " AND EMP_NAME.ATTR_ID = 5  /* NAME*/ " +
-            " AND EMP_EMAIL.VALUE = ? /* MAIL*/ " +
-            " AND EMP_NAME.OBJECT_ID = EMP.OBJECT_ID  " +
-            " AND EMP_EMAIL.OBJECT_ID = EMP.OBJECT_ID  " +
+    String GET_USER_BY_LOGIN = "SELECT US.name obj_name , EMP_NAME.value name,  US_EMAIL.VALUE email,US.OBJECT_ID AS USER_ID, " +
+            "PASSWORD.VALUE AS PASSWORD, EMP_STATUS.LIST_VALUE_ID AS IS_ACTIVE, " +
+            "(SELECT PER_ACC.REFERENCE from OBJREFERENCE PER_ACC where US.OBJECT_ID  = PER_ACC.OBJECT_ID AND PER_ACC.ATTR_ID = 1) as per_deb_acc1, " +
+            "(SELECT FAM_ACC.REFERENCE from OBJREFERENCE FAM_ACC where US.OBJECT_ID  = FAM_ACC.OBJECT_ID AND FAM_ACC.ATTR_ID = 2) as fam_deb_acc1 " +
+            "FROM OBJECTS US, ATTRIBUTES US_EMAIL, ATTRIBUTES PASSWORD, ATTRIBUTES EMP_STATUS, attributes EMP_NAME " +
+            "WHERE US_EMAIL.VALUE  = ? " +
+            " and US.OBJECT_TYPE_ID = 1 " +
+            " AND US.OBJECT_ID = US_EMAIL.OBJECT_ID " +
             " AND PASSWORD.ATTR_ID = 4  /* PASSWORD*/ " +
-            " AND PASSWORD.OBJECT_ID = EMP.OBJECT_ID  " +
+            " AND PASSWORD.OBJECT_ID = US.OBJECT_ID " +
             " AND EMP_STATUS.ATTR_ID = 6  /* IS_ACTIVE*/ " +
-            " AND EMP_STATUS.OBJECT_ID = EMP.OBJECT_ID  " +
-            " AND PER_US.ATTR_ID = 1  /* OWNER_PERSONAL_ACC */ " +
-            " AND PER_US.OBJECT_ID = EMP.OBJECT_ID  " +
-            " AND PER.OBJECT_ID = PER_US.REFERENCE   " +
-            " AND FAM_US.ATTR_ID = 2  /* OWNER_FAMILY_ACC */ " +
-            " AND FAM_US.OBJECT_ID = EMP.OBJECT_ID  " +
-            " AND FAM.OBJECT_ID = FAM_US.REFERENCE ";
-
+            " AND EMP_STATUS.OBJECT_ID = US.OBJECT_ID " +
+            " AND EMP_NAME.ATTR_ID = 5  /* NAME*/ " +
+            " AND EMP_NAME.OBJECT_ID = US.OBJECT_ID ";
 
     String GET_USER_BY_USER_ID = "SELECT EMP.OBJECT_ID AS USER_ID, EMP_NAME.VALUE AS NAME, EMP_EMAIL.VALUE AS EMAIL, " +
             "PASSWORD.VALUE AS PASSWORD, EMP_STATUS.LIST_VALUE_ID AS IS_ACTIVE, PER.OBJECT_ID AS PER_DEB_ACC1, FAM.OBJECT_ID AS FAM_DEB_ACC1 " +
--- tests/com/netcracker/dao/impl/UserDaoTest.java
+++ tests/com/netcracker/dao/impl/UserDaoTest.java
@@ -63,7 +63,7 @@ public class UserDaoTest {
 
     @Test
     public void getUserByLogin(){
-        User user = userDao.getUserByEmail("name123kdmwel;d");
+        User user = userDao.getUserByEmail("mailDimas@gmail.com");
         assertEquals("user_id=24 ListStatus=YES name=Dimas email=mailDimas@gmail.com password=passwordDima personal_Acc=25 familyAcc=26",
                 "user_id=" + user.getId() + " ListStatus=" + user.getUserStatusActive() +
                         " name=" + user.getName() + " email=" + user.geteMail() +
