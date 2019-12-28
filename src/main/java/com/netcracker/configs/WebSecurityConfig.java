cbb7684ebf71723a460bc13ae7596ec40afb0ebf
Katerina <malutina.catya14@gmail.com>
2019-12-28 08:59:42 +0200

    [OD19DEVAT1-67] Spring security database config

--- src/main/java/com/netcracker/configs/WebSecurityConfig.java
+++ src/main/java/com/netcracker/configs/WebSecurityConfig.java
@@ -1,11 +1,15 @@
 package com.netcracker.configs;
 
+import com.netcracker.dao.UserDao;
+import com.netcracker.services.impl.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -15,11 +19,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     PasswordEncoder passwordEncoder;
 
+    @Autowired
+    UserDao userDao;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(passwordEncoder)
-                .withUser("user").password(passwordEncoder.encode("pas")).roles("USER");
+        auth.userDetailsService(new UserDetailsServiceImpl(userDao)).passwordEncoder(passwordEncoder);
     }
 
     @Bean
@@ -27,16 +32,30 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsServiceImpl(userDao);
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder);
+        return authProvider;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http
+                .authorizeRequests()
+                .antMatchers("/resources/**", "/registration").permitAll()
                 .anyRequest().authenticated()
-                .and().httpBasic()
-//                .antMatchers("/**").permitAll()
-                .and().csrf().disable();
-
-
-//
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .usernameParameter("email")
+                .permitAll();
     }
 
 }
