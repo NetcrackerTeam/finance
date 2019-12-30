183c2f9a68cd11770fdef2c08e693fc362ed9399
Katerina <malutina.catya14@gmail.com>
2019-12-30 12:14:16 +0200

    delete comment

--- src/main/java/com/netcracker/configs/SecurityInit.java
+++ src/main/java/com/netcracker/configs/SecurityInit.java
@@ -3,29 +3,4 @@ package com.netcracker.configs;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
 public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
-    //                .antMatchers("/resources/**", "/registration").permitAll()
-////                .authorizeRequests()
-////                .antMatchers("/**").hasRole("USER")
-////                .and()
-//                .formLogin()
-//                .loginPage("/login")
-////                .usernameParameter("username")
-////                .passwordParameter("password")
-//                .permitAll()
-//                .and()
-//                .antMatcher("/registration")
-////                .anyRequest().authenticated()
-////                .and()
-////                .httpBasic()
-////                .and()
-////                .formLogin()
-////                .loginPage("/login")
-////                .usernameParameter("username")
-////                .passwordParameter("password").permitAll()
-////                .loginProcessingUrl("/doLogin")
-////            .successForwardUrl("/")
-////                .httpBasic()
-////                .antMatchers("/**").permitAll()
-//                .and().csrf().disable();
-
 }
