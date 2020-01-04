53309f7ab152aa6cf7d7860fcf0f7a957d0958d9
Andrew Kompaniets <44323920+kiillthis@users.noreply.github.com>
2020-01-04 16:37:24 +0200

    [OD19DEVAT1-96] - fix family prediction

--- src/main/webapp/resources/js/controllers/PersonalPredictController.js
+++ src/main/webapp/resources/js/controllers/PersonalPredictController.js
@@ -38,3 +38,42 @@ app.controller("PersonalPredictController", ['$scope', '$http', '$templateCache'
             $scope.url = url;
         };
     }]);
+
+app.controller("FamilyPredictController", ['$scope', '$http', '$templateCache',
+    function($scope, $http, $templateCache) {
+        $scope.method = 'GET';
+        $scope.url = null;
+
+        $scope.predict = {
+            duration: null,
+            category: null
+        };
+
+        $scope.fetch = function() {
+            $scope.code = null;
+            $scope.response = null;
+
+            if($scope.predict.category == 'Income') {
+                $scope.url = 'prediction/3/family/income';
+            } else {
+                $scope.url = 'prediction/3/family/expense';
+            }
+
+            $http({
+                method: $scope.method,
+                url: $scope.url,
+                params: {duration: $scope.predict.duration}
+            }).
+            then(function(response) {
+                $scope.status = response.data;
+            }, function(response) {
+                $scope.data = response.data || 'Request failed';
+                $scope.status = response.status;
+            });
+        };
+
+        $scope.updateModel = function(method, url) {
+            $scope.method = method;
+            $scope.url = url;
+        };
+    }]);
